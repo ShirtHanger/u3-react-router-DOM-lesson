03990e1829584640e4ebb052ac0fdbd697313c40
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-11-04 11:30:50 -0600

    part 1 done

--- react-router-dom/src/main.jsx
+++ react-router-dom/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom'; /* Enables react page to update URL and display different pages WITHOUT URL refresh */
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Wrap this in BrowserRouter component tags, to enable it! */}
+    {/* Passes functionality of BrowserRouter into the entire webpage! */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
